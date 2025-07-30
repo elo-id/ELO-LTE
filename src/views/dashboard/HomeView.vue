3ff342aac47b74fd5dce1ff5cf45f7c5502572ae
Raphaelle <raveraph74@gmail.com>
2025-07-30 17:16:57 +0700

    landing page

--- src/views/dashboard/HomeView.vue
+++ src/views/dashboard/HomeView.vue
@@ -1,8 +1,38 @@
 <template>
-  <div class="home">
-    Template Dashboard ELO
+  <div class="landing-page">
+    <div class="content">
+      <h1>ELO Digital Office Indonesia</h1>
+      <img src="@/assets/elo-heart.png" alt="AT THE HEART OF YOUR BUSINESS" class="banner" />
+    </div>
   </div>
 </template>
 
 <script>
+export default {
+  name: 'LandingPage',
+};
 </script>
+
+<style scoped>
+.landing-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 80px);
+  text-align: center;
+  flex-direction: column;
+  box-sizing: border-box;
+  margin-top: -40px;
+}
+
+h1 {
+  color: var(--text);
+  font-size: 6rem;
+  margin-bottom: 1rem;
+}
+
+.banner {
+  max-width: 50%;
+  height: auto;
+}
+</style>
